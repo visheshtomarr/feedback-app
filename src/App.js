2840023b3bbcc5316661490d0693a48b373762c7
visheshtomarr <visheshtomar7800@gmail.com>
2024-09-17 19:15:43 +0530

    Added styling for App component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from "./Components/Header";
 function App() {
     return (
         <>
-            <Header text={"Feedback UI"} />
+            <Header text="Feedback UI" bgColor="rgba(0,0,0,0.3)" />
             <div className="container">
                 <h1>My App</h1>
             </div>
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,18 +1,23 @@
 import PropTypes from 'prop-types';
 
-function Header({ text }) {
-  return (
-    <header>
-        <div className="container">
-            <h2>{text}</h2>
-        </div>
-    </header>
-  )
+function Header({ text, bgColor }) {
+    const headerStyles = {
+        backgroundColor: bgColor, 
+    }
+
+    return (
+        <header style={headerStyles}>
+            <div className="container">
+                <h2>{text}</h2>
+            </div>
+        </header>
+    )
 }
 
-// Type of prop that can be passed to 'Header' component.
+// Type of props that can be passed to 'Header' component.
 Header.protoTypes = {
     text: PropTypes.string,
+    bgColor: PropTypes.string,
 }
 
 export default Header;
